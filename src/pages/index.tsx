3d2287987471384f8835ffc04ca3cb991fa053bb
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-09-21 07:39:58 -0300

    finish header first part

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,24 +12,21 @@ const Home: NextPage = () => {
         <title>Mateus Alcantara</title>
       </Head>
       <Header />
-      <Flex
-        pt="100px"
-        minHeight="5000px"
-        position="relative"
-        fontFamily="Roboto Mono"
-      >
+      <Flex pt="100px" minHeight="5000px" position="relative">
         <Flex direction="column" mt={16} mx="auto">
-          <Text color="primaryBlue" fontWeight="600">
+          <Text color="primaryBlue" fontWeight="600" fontFamily="Roboto Mono">
             Oi, meu nome é
           </Text>
-          <Heading color="gray.50" mt={4}>
+          <Heading lineHeight="68px" size="3xl" color="gray.50" mt={4}>
             Mateus Alcantara.
           </Heading>
-          <Heading color="gray.100">Eu sou um Desenvolvedor Fullstack</Heading>
-          <Text maxW="540px" color="gray.200" mt={4}>
-            Apaixonado por tecnologia e programação. Eu gosto de usar minhas
+          <Heading lineHeight="68px" size="3xl" color="gray.100">
+            Eu sou um Desenvolvedor Fullstack
+          </Heading>
+          <Text maxW="540px" color="gray.200" mt={4} fontSize="20px">
+            Apaixonado por tecnologia e programação, eu gosto de usar minhas
             habilidades para criar produtos que tem o potencial de tornar a vida
-            das pessoas mais fáceis.
+            das pessoas mais fáceis. Atualmente desenvolvo como autonomo.
           </Text>
         </Flex>
       </Flex>
