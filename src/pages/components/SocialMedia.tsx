e3356feb48ff5efe2c1d5697d412babcf326b3a7
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-09-22 07:23:50 -0300

    add direct link to social medias

--- src/pages/components/SocialMedia.tsx
+++ src/pages/components/SocialMedia.tsx
@@ -1,7 +1,12 @@
-import { Divider, Flex, Text } from '@chakra-ui/react';
+import { Divider, Flex, IconButton, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { SiHackerrank } from 'react-icons/si';
 
 export const SocialMedia: React.FC = () => {
+  const { push } = useRouter();
+
   return (
     <Flex
       position="fixed"
@@ -11,14 +16,46 @@ export const SocialMedia: React.FC = () => {
       bottom="0px"
       fontFamily="Roboto Monospace"
     >
-      <Flex direction="column" align="center">
-        <Divider h="100px" orientation="vertical" />
+      <Flex direction="column" align="center" bottom="0px">
+        <IconButton
+          mt={8}
+          colorScheme="transparent"
+          aria-label="Github"
+          icon={<FaGithub fontSize="25px" />}
+          onClick={() => push('https://github.com/mat-alcantara')}
+        />
+        <IconButton
+          mt={8}
+          colorScheme="transparent"
+          aria-label="Linkedin"
+          icon={<FaLinkedin fontSize="25px" />}
+          onClick={() => push('https://www.linkedin.com/in/mat-alcantara/')}
+        />
+        <IconButton
+          mt={8}
+          colorScheme="transparent"
+          aria-label="Hacker Rank"
+          icon={<SiHackerrank fontSize="25px" />}
+          onClick={() => push('https://www.hackerrank.com/mateuscastro_al')}
+        />
+        <IconButton
+          mt={8}
+          colorScheme="transparent"
+          aria-label="Instagram"
+          icon={<FaInstagram fontSize="25px" />}
+          onClick={() => push('https://www.instagram.com/mat.alcantara/')}
+        />
+        <Divider mt={8} h="100%" minH="100px" orientation="vertical" />
       </Flex>
       <Flex direction="column" align="center">
-        <Text letterSpacing="0.1em" sx={{ writingMode: 'tb-rl' }}>
+        <Text
+          fontSize="20px"
+          letterSpacing="0.1em"
+          sx={{ writingMode: 'tb-rl' }}
+        >
           mateuscastro.al@gmail.com
         </Text>
-        <Divider mt={4} h="100px" orientation="vertical" />
+        <Divider mt={8} h="100%" minH="100px" orientation="vertical" />
       </Flex>
     </Flex>
   );
