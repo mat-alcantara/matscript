ca00784c095d46fe6788b34035dd4112dac602f0
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-09-22 07:30:07 -0300

    Resize social media links

--- src/pages/components/SocialMedia.tsx
+++ src/pages/components/SocialMedia.tsx
@@ -16,36 +16,44 @@ export const SocialMedia: React.FC = () => {
       bottom="0px"
       fontFamily="Roboto Monospace"
     >
-      <Flex direction="column" align="center" bottom="0px">
-        <IconButton
-          mt={8}
-          colorScheme="transparent"
-          aria-label="Github"
-          icon={<FaGithub fontSize="25px" />}
-          onClick={() => push('https://github.com/mat-alcantara')}
-        />
-        <IconButton
-          mt={8}
-          colorScheme="transparent"
-          aria-label="Linkedin"
-          icon={<FaLinkedin fontSize="25px" />}
-          onClick={() => push('https://www.linkedin.com/in/mat-alcantara/')}
-        />
-        <IconButton
-          mt={8}
-          colorScheme="transparent"
-          aria-label="Hacker Rank"
-          icon={<SiHackerrank fontSize="25px" />}
-          onClick={() => push('https://www.hackerrank.com/mateuscastro_al')}
-        />
-        <IconButton
-          mt={8}
-          colorScheme="transparent"
-          aria-label="Instagram"
-          icon={<FaInstagram fontSize="25px" />}
-          onClick={() => push('https://www.instagram.com/mat.alcantara/')}
-        />
-        <Divider mt={8} h="100%" minH="100px" orientation="vertical" />
+      <Flex direction="column" align="center">
+        <Flex
+          direction="column"
+          h="100%"
+          align="center"
+          justify="space-between"
+        >
+          <IconButton
+            mt={4}
+            colorScheme="transparent"
+            aria-label="Github"
+            icon={<FaGithub fontSize="20px" />}
+            onClick={() => push('https://github.com/mat-alcantara')}
+          />
+          <IconButton
+            mt={4}
+            colorScheme="transparent"
+            aria-label="Linkedin"
+            icon={<FaLinkedin fontSize="20px" />}
+            onClick={() => push('https://www.linkedin.com/in/mat-alcantara/')}
+          />
+          <IconButton
+            mt={4}
+            colorScheme="transparent"
+            aria-label="Hacker Rank"
+            icon={<SiHackerrank fontSize="20px" />}
+            onClick={() => push('https://www.hackerrank.com/mateuscastro_al')}
+          />
+          <IconButton
+            mt={4}
+            colorScheme="transparent"
+            aria-label="Instagram"
+            icon={<FaInstagram fontSize="20px" />}
+            onClick={() => push('https://www.instagram.com/mat.alcantara/')}
+          />
+        </Flex>
+
+        <Divider mt={4} minH="100px" h="100%" orientation="vertical" />
       </Flex>
       <Flex direction="column" align="center">
         <Text
@@ -55,7 +63,7 @@ export const SocialMedia: React.FC = () => {
         >
           mateuscastro.al@gmail.com
         </Text>
-        <Divider mt={8} h="100%" minH="100px" orientation="vertical" />
+        <Divider mt={4} minH="100px" h="100%" orientation="vertical" />
       </Flex>
     </Flex>
   );
