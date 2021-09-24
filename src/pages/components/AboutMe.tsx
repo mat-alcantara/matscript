9adb3aba53ed18a074df1a9406e5e58669cd5d40
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-09-24 19:46:54 -0300

    Make text float with image

--- src/pages/components/AboutMe.tsx
+++ src/pages/components/AboutMe.tsx
@@ -12,7 +12,7 @@ import React from 'react';
 export const AboutMe: React.FC = () => {
   return (
     <Flex direction="column" justify="center" w="100%">
-      <Flex align="center" w="100%">
+      <Flex align="center">
         <Heading
           whiteSpace="nowrap"
           size="lg"
@@ -25,23 +25,30 @@ export const AboutMe: React.FC = () => {
       </Flex>
       <Flex mt={16} align="flex-start">
         <VStack>
-          <Text>
+          <Text float="left" textAlign="justify">
+            <Image
+              float="right"
+              w="400px"
+              src="./profile2.jpg"
+              height="auto"
+              borderRadius="4px"
+              ml={8}
+              mb={8}
+            />
             Meu nome é Mateus Alcantara, eu tenho 24 anos e eu sou
             Desenvolvedor. A minha paixão por programação começou em 2016,
             quando ainda na faculdade de Engenharia Elétrica eu tive o meu
             primeiro contato com a linguagem C++. Infelizmente por problemas
             financeiros eu não pude dar continuidade ao meu curso, porém o amor
-            por programação nunca saiu de mim.
-          </Text>
-          <Box />
-          <Text>
+            por programação nunca saiu de mim. <br />
+            <br />
             Após esse período, passei a trabalhar em uma empresa familiar que se
             encontrava a beira da falềncia. Nesta empresa, fundamos o setor de
             Marcenaria, onde fiquei responsável tanto pela parte gerencial,
             quanto pelo trabalho braçal. Em 3 anos nós expandimos o setor ao
             ponto de se tornar o setor mais lucrativo da empresa.
-          </Text>
-          <Text>
+            <br />
+            <br />
             Apesar da rápida expansão, enfrentamos muitos problemas gerenciais,
             principalmente pela dificuldade na comunicação entre as lojas e a
             Marcenaria. Foi então que eu decidi que resolveria todos esses
@@ -51,8 +58,8 @@ export const AboutMe: React.FC = () => {
             adquiridos como autodidata, eu criei o site que hoje gerencia todos
             os pedidos da Marcenaria, e com o tempo acrescentei ferramentas para
             facilitar outros setores da empresa.
-          </Text>
-          <Text>
+            <br />
+            <br />
             Já são mais de 2 anos completamente apaixonado pelo que eu faço,
             acordando todos os dias às 5AM para poder estudar programação antes
             do trabalho, utilizando os meus conhecimentos para resolver
@@ -63,11 +70,8 @@ export const AboutMe: React.FC = () => {
             de obter aprendizado ao mesmo tempo em que eu possa ajudar a
             construir projetos que farão do mundo um lugar melhor.
           </Text>
-          <Text />
         </VStack>
-        <Flex ml={16} align="center">
-          <Image src="./profile2.jpg" height="auto" borderRadius="4px" />
-        </Flex>
+        <Flex ml={16} align="center" />
       </Flex>
     </Flex>
   );
