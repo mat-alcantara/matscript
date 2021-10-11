57c3c8b104d75bccedccc25c88431448402a302a
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-11 06:02:36 -0300

    change PostItem font sizes

--- src/components/blog/Post/PostItem.tsx
+++ src/components/blog/Post/PostItem.tsx
@@ -1,4 +1,12 @@
-import { Box, Flex, Heading, HStack, Icon, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 import React from 'react';
 import { FaRegCalendar, FaRegClock } from 'react-icons/fa';
 
@@ -9,20 +17,25 @@ interface PostItemProps {
 }
 
 export const PostItem: React.FC<PostItemProps> = ({ post }) => {
+  const headingSize = useBreakpointValue(['sm', 'lg']);
+  const descriptionSize = useBreakpointValue(['14px', '16px']);
+
   return (
     <Flex direction="column">
       <Box as="a" href={`/post/${post.slug}`}>
-        <Heading size="lg">{post.title}</Heading>
+        <Heading size={headingSize}>{post.title}</Heading>
       </Box>
 
       <HStack spacing={8} mt={2}>
         <HStack>
           <Icon as={FaRegCalendar} />
-          <Text>{post.createdAt}</Text>
+          <Text fontSize={descriptionSize}>{post.createdAt}</Text>
         </HStack>
         <HStack>
           <Icon as={FaRegClock} />
-          <Text>{`${post.estimateReadingTime} minutos de leitura`}</Text>
+          <Text
+            fontSize={descriptionSize}
+          >{`${post.estimateReadingTime} minutos de leitura`}</Text>
         </HStack>
       </HStack>
     </Flex>
