43c5ef01e23df9d76a679809241b2c092fbf7cc4
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-11 06:20:15 -0300

    finish index

--- src/components/blog/Footer.tsx
+++ src/components/blog/Footer.tsx
@@ -1,32 +1,43 @@
-import { Flex, HStack, IconButton, Text } from '@chakra-ui/react';
+import {
+  Flex,
+  HStack,
+  IconButton,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 import Router from 'next/router';
 import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 export const Footer: React.FC = () => {
   const { push } = Router;
+  const logoSize = useBreakpointValue(['18px', '24px']);
+  const iconButtonSize = useBreakpointValue(['sm', 'md', 'lg']);
+  const copySize = useBreakpointValue(['12px', '']);
 
   return (
     <Flex as="footer" justify="space-between" pb={8} pt={16} align="center">
-      <Text>Copyright © Mateus Alcantara</Text>
+      <Text fontSize={copySize} size="">
+        Copyright © Mateus Alcantara
+      </Text>
       <HStack spacing={[0, 2]}>
         <IconButton
-          size="lg"
+          size={iconButtonSize}
           aria-label="Github"
-          icon={<FaGithub fontSize="24px" />}
+          icon={<FaGithub fontSize={logoSize} />}
           variant="ghost"
           onClick={() => push('https://github.com/mat-alcantara')}
         />
         <IconButton
-          size="lg"
+          size={iconButtonSize}
           aria-label="Linkedin"
-          icon={<FaLinkedin fontSize="24px" />}
+          icon={<FaLinkedin fontSize={logoSize} />}
           variant="ghost"
           onClick={() => push('https://www.linkedin.com/in/mat-alcantara/')}
         />
         <IconButton
-          size="lg"
+          size={iconButtonSize}
           aria-label="Instagram"
-          icon={<FaInstagram fontSize="24px" />}
+          icon={<FaInstagram fontSize={logoSize} />}
           variant="ghost"
           onClick={() => push('https://www.instagram.com/mat.alcantara/')}
         />
